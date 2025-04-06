ef0373ca3056cfc33a95665a21d111f95956125e
doxik3888 <153354996+doxik3888@users.noreply.github.com>
2025-04-06 22:27:48 +0300

    Add files via upload

---
+++ 3_str/scripts.js
@@ -0,0 +1,16 @@
+let btn_1 = document.getElementById("1")
+btn_1.addEventListener("click", () => {
+    location.href = "../index.html"
+})
+let btn_2 = document.getElementById("2")
+btn_2.addEventListener("click", () => {
+    location.href = "../2_str/index.html"
+})
+let btn_4 = document.getElementById("4")
+btn_4.addEventListener("click", () => {
+    location.href = "../4_str/index.html"
+})
+let btn_5 = document.getElementById("5")
+btn_5.addEventListener("click", () => {
+    location.href = "../5_str/index.html"
+})
---
+++ 5_str/scripts.js
@@ -0,0 +1,16 @@
+let btn_1 = document.getElementById("1")
+btn_1.addEventListener("click", () => {
+    location.href = "../index.html"
+})
+let btn_2 = document.getElementById("2")
+btn_2.addEventListener("click", () => {
+    location.href = "../2_str/index.html"
+})
+let btn_3 = document.getElementById("3")
+btn_3.addEventListener("click", () => {
+    location.href = "../3_str/index.html"
+})
+let btn_4 = document.getElementById("4")
+btn_4.addEventListener("click", () => {
+    location.href = "../4_str/index.html"
+})
